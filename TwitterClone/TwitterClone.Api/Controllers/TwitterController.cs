7590913348b5d0667677af0ca7ee72247c8568fd
tan45Nadim <tan45.nadim@gmail.com>
2026-09-17 00:21:04 +0600

    connect webapi

---
+++ TwitterClone/TwitterClone.Api/Controllers/TwitterController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TwitterClone.Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class TwitterController : ControllerBase
+{
+    private readonly IConfiguration _configuration;
+
+    public TwitterController(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    [HttpGet("tweets")]
+    public IActionResult GetTweets()
+    {
+        var tweets = new List<object>
+        {
+            new
+            {
+                UserId = Guid.NewGuid(),
+                Content = "1st tweet"
+            },
+            new
+            {
+                UserId = Guid.NewGuid(),
+                Content = "2nd tweet"
+            },
+        };
+
+        return Ok(tweets);
+    }
+
+
+    [HttpGet("app-info")]
+    public IActionResult GetAppInfo()
+    {
+        var appName = _configuration.GetValue<string>("Logging:LogLevel:Default");
+
+        return Ok(new { AppName = appName });
+    }
+
+}
